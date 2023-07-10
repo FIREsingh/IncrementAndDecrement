6c011008ad7c9cd2e59de2595c0221360850e787
FIREsingh <manishaithani67@gmail.com>
2023-07-10 17:09:08 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "./App.css";
+import TextBox from "./containers/TextBox";
+
+function App() {
+  const [num,setNum] = useState(0)
+  function incHandler(){
+    setNum(num+1)
+  }
+  function decHandler(){
+    setNum(num-1)
+  }
+  function resetHandler(){
+    setNum(0)
+  }
+  return (
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344152] flex-col gap-15 space-y-9">
+      <div> <h1 className=" text-[#0398d4] font-medium text-2xl " >Increasing And Decreasing</h1></div>
+
+      <div className="bg-white flex justify-center gap-12 py-3 text-[25px] rounded-lg" >
+        <button onClick={decHandler} className="font-bold text-3xl border-r-2 text-center w-20 "> - </button>
+
+        <div className=' font-bold text-3xl' > {num} </div>
+
+        <button onClick={incHandler} className=" font-bold text-3xl border-l-2 text-center w-20 " > + </button> 
+      </div>
+
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-all delay-50 m-10" onClick={resetHandler} >Reset</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/TextBox.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TextBox() {
+  return (
+    <div>
+      <input type="text" className='border border-red-2'/>
+    </div>
+  )
+}
